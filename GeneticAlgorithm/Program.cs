7d973b5d07d44b22fcf03d313547323f7cbd71bc
Pinacolada <mdcmatheusmdc@gmail.com>
2022-07-13 23:08:57 -0300

    Adding BruteForce Algorithm

--- GeneticAlgorithm/Program.cs
+++ GeneticAlgorithm/Program.cs
@@ -5,128 +5,163 @@ using GeneticAlgorithm;
 using GeneticAlgorithm.Models;
 using ScottPlot;
 using Utils;
+// ReSharper disable InvalidXmlDocComment
+
 
 const int popSize = 100;
 const int maxIterations = 1000;
-const string filePath = "Files/n_1200_c_10000000000_g_14_f_0.1_eps_0.1_s_200.in";
+const string pathPrefix = "Files/dataset";
+
+var availableDataSetFiles = new List<double>() { 5, 10, 15, 20, 25, 30 };
 
 Console.WriteLine("===== Execution Started =====");
 Console.WriteLine();
 
-var (items, knapsackMaxWeight) = KnapsackDataReader.GetKnapsackData(filePath);
-var knapsackItems = items.OrderBy(x => x.PriceToWeightRatio)
-                         .Select((x, i) => (index: i, item: x))
-                         .ToDictionary(x => x.index, x => x.item);
-
-if(knapsackItems is null)
-    throw new($"Error, '{nameof(knapsackItems)}' cannot be null");
-
-var availableItemsQty = knapsackItems.Count;
-
-var initialPop = Population.GenerateInitialPopulation(
-    popSize,
-    new() { ChromosomeSize = knapsackItems.Count },
-    (_, configuration) =>
-    {
-        var random = new Random();
-        var randomChromosome = Enumerable.Range(0, configuration.ChromosomeSize)
-                                    .Select(x => (object)(random.NextDouble() < 0.3))
-                                    .ToList();
-        var newInd = new Individual() { Chromosome = randomChromosome };
-
-        ApplyWeightRestriction(newInd);
-
-        return newInd;
-    });
-
-
-var genetic = new GeneticAlgorithm.GeneticAlgorithm(initialPop)
-{
-    MutationRate = 0.2,
-    ElitismQty = 1,
-    EvaluationFunc = (x) => GetSackItems(x).Sum(chrom => chrom.item.Price),
-    CrossOverFunc = (parent1, parent2) =>
-    {
-        var rnd = new Random();
-        var childs = parent1.Chromosome.Zip(parent2.Chromosome, (p1, p2) => rnd.NextDouble() < 0.5
-                                                                                ? (p1, p2)
-                                                                                : (p2, p1)
-            ).AsList()!;
-
-        var child1 = new Individual() { Chromosome = childs.Select(x => x.Item1).ToList() };
-        var child2 = new Individual() { Chromosome = childs.Select(x => x.Item1).ToList() };
-
-        ApplyWeightRestriction(child1);
-        ApplyWeightRestriction(child2);
-
-        return (child1, child2);
-    },
-    MutationFunc = (individual) =>
-    {
-        var rnd = new Random();
-        individual.Chromosome = individual.Chromosome
-                                          .Select((x, i) => rnd.NextDouble() < (i + 1) / (availableItemsQty * 100.0) ? !(bool)x : x)
-                                          .ToList();
-        ApplyWeightRestriction(individual);
-        return individual;
-    }
-};
-
-var plotXValues = new List<double>();
-var plotYValues = new List<double>();
-
-var timeWatch = new Stopwatch();
-timeWatch.Start();
-for(var i = 0; i < maxIterations; i++)
-{
-    genetic.GenerateNewPopulation();
-
-    plotXValues.Add(i + 1);
-    plotYValues.Add(genetic.ProcessedIndividuals.First().Value);
-}
-timeWatch.Stop();
+var plotYValues = availableDataSetFiles
+                  .Select(availableDataSet => RunGeneticForDataSet($"{pathPrefix}{availableDataSet}").TotalSeconds)
+                  .ToList();
 
 var plt = new ScottPlot.Plot(1600, 900);
-plt.AddScatter(plotXValues.ToArray(), plotYValues.ToArray(), lineStyle: LineStyle.None, label: "Best Individual per iteration");
-plt.SaveFig($"../../../../Images/{filePath.Split("/").Last()}-ValueProgression.png");
-
-var bestIndividual = genetic.ProcessedIndividuals.First();
-Console.WriteLine($"Best Value found was: {bestIndividual.Value}");
-var itemsInBestSack = GetSackItems(bestIndividual.Individual).OrderBy(x => Convert.ToInt32(x.item.Label)).AsList()!;
-foreach(var itemWithIndex in itemsInBestSack)
-    Console.WriteLine($" => Label: {itemWithIndex.item.Label} - Price: {itemWithIndex.item.Price} - Weight: {itemWithIndex.item.Weight}");
-Console.WriteLine($"Weight of best sack: {itemsInBestSack.Sum(x => x.item.Weight)}");
-Console.WriteLine($"Execution Elapsed Time: {timeWatch.Elapsed.TotalSeconds} seconds");
+plt.Title("Genetic Execution Time (Seconds)");
+plt.AddScatter(availableDataSetFiles.ToArray(), plotYValues.ToArray(), lineStyle: LineStyle.DashDotDot, label: "Genetic Execution Time (Seconds)");
+plt.SaveFig("../../../../Images/Genetic/GeneticExecutionTime.png");
 
 Console.WriteLine();
 Console.WriteLine("===== Execution Ended =====");
 
-// ======================================
+// ====================================================
 
-IEnumerable<(int index, KnapsackItem item)> GetSackItems(Individual ind) =>
-    ind.Chromosome
-       .Select((inSack, i) => ((bool)inSack) ? (index: i, item: knapsackItems![i]) : default)
-       .Where(x => x != default);
-
-void ApplyWeightRestriction(Individual ind)
+TimeSpan RunGeneticForDataSet(string path)
 {
-    var weights = GetSackItems(ind)
-                  .Where(x => x != default)
-                  .AsList()!;
-    var totalWeight = weights.Sum(x => x.item.Weight);
+    /** ===========================================
+     *  Reading data from File
+        ===========================================*/
+    var (items, knapsackMaxWeight) = KnapsackDataReader.GetKnapsackData(path);
+    var knapsackItems = items.OrderBy(x => x.PriceToWeightRatio)
+                             .Select((x, i) => (index: i, item: x))
+                             .ToDictionary(x => x.index, x => x.item);
 
-    if(totalWeight <= knapsackMaxWeight)
-        return;
+    if(knapsackItems is null)
+        throw new($"Error, '{nameof(knapsackItems)}' cannot be null");
 
-    var extraWeight = totalWeight - knapsackMaxWeight;
-    var removedWeight = 0.0;
-    var toRemoveItems = weights.TakeWhile(x =>
+    var availableItemsQty = knapsackItems.Count;
+
+    /** =========================================== */
+
+    var initialPop = Population.GenerateInitialPopulation(
+        popSize,
+        new() { ChromosomeSize = knapsackItems.Count },
+        (_, configuration) =>
+        {
+            var random = new Random();
+            var randomChromosome = Enumerable.Range(0, configuration.ChromosomeSize)
+                                        .Select(x => (object)(random.NextDouble() < 0.3))
+                                        .ToList();
+            var newInd = new Individual() { Chromosome = randomChromosome };
+
+            ApplyWeightRestriction(newInd);
+
+            return newInd;
+        });
+
+
+    var genetic = new GeneticAlgorithm.GeneticAlgorithm(initialPop)
     {
-        var decision = extraWeight > removedWeight;
-        removedWeight += x.item.Weight;
-        return decision;
-    });
+        MutationRate = 0.2,
+        ElitismQty = 1,
+        EvaluationFunc = (x) => GetSackItems(x).Sum(chrom => chrom.item.Price),
+        CrossOverFunc = (parent1, parent2) =>
+        {
+            var rnd = new Random();
+            var childs = parent1.Chromosome.Zip(parent2.Chromosome, (p1, p2) => rnd.NextDouble() < 0.5
+                                                                                    ? (p1, p2)
+                                                                                    : (p2, p1)
+                ).AsList()!;
 
-    foreach(var toRemoveItem in toRemoveItems)
-        ind.Chromosome[toRemoveItem.index] = false;
+            var child1 = new Individual() { Chromosome = childs.Select(x => x.Item1).ToList() };
+            var child2 = new Individual() { Chromosome = childs.Select(x => x.Item1).ToList() };
+
+            ApplyWeightRestriction(child1);
+            ApplyWeightRestriction(child2);
+
+            return (child1, child2);
+        },
+        MutationFunc = (individual) =>
+        {
+            var rnd = new Random();
+            individual.Chromosome = individual.Chromosome
+                                              .Select((x, i) => rnd.NextDouble() < (i + 1) / (availableItemsQty * 100.0) ? !(bool)x : x)
+                                              .ToList();
+            ApplyWeightRestriction(individual);
+            return individual;
+        }
+    };
+
+    var plotXValues = new List<double>();
+    var plotYValues = new List<double>();
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    for(var i = 0; i < maxIterations; i++)
+    {
+        genetic.GenerateNewPopulation();
+
+        plotXValues.Add(i + 1);
+        plotYValues.Add(genetic.ProcessedIndividuals.First().Value);
+    }
+    stopwatch.Stop();
+
+    var plt = new ScottPlot.Plot(1600, 900);
+    plt.AddScatter(plotXValues.ToArray(), plotYValues.ToArray(), lineStyle: LineStyle.None, label: "Best Individual per iteration");
+    plt.SaveFig($"../../../../Images/Genetic/{path.Split("/").Last()}-ValueProgression.png");
+
+    var bestIndividual = genetic.ProcessedIndividuals.First();
+
+    Console.WriteLine("");
+    Console.WriteLine($"Best Value found was: {bestIndividual.Value}");
+    var itemsInBestSack = GetSackItems(bestIndividual.Individual).OrderBy(x => Convert.ToInt32(x.item.Label)).AsList()!;
+    foreach(var itemWithIndex in itemsInBestSack)
+        Console.WriteLine($" => Label: {itemWithIndex.item.Label} - Price: {itemWithIndex.item.Price} - Weight: {itemWithIndex.item.Weight}");
+    Console.WriteLine($"Weight of best sack: {itemsInBestSack.Sum(x => x.item.Weight)}");
+    Console.WriteLine($"Execution Elapsed Time: {stopwatch.Elapsed.TotalSeconds} seconds");
+    Console.WriteLine("");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(" ================================= ");
+    Console.ResetColor();
+    Console.WriteLine("");
+
+    return stopwatch.Elapsed;
+
+
+    // ====================================================
+
+    IEnumerable<(int index, KnapsackItem item)> GetSackItems(Individual ind) =>
+        ind.Chromosome
+           .Select((inSack, i) => ((bool)inSack) ? (index: i, item: knapsackItems![i]) : default)
+           .Where(x => x != default);
+
+    void ApplyWeightRestriction(Individual ind)
+    {
+        var weights = GetSackItems(ind)
+                      .Where(x => x != default)
+                      .AsList()!;
+        var totalWeight = weights.Sum(x => x.item.Weight);
+
+        if(totalWeight <= knapsackMaxWeight)
+            return;
+
+        var extraWeight = totalWeight - knapsackMaxWeight;
+        var removedWeight = 0.0;
+        var toRemoveItems = weights.TakeWhile(x =>
+        {
+            var decision = extraWeight > removedWeight;
+            removedWeight += x.item.Weight;
+            return decision;
+        });
+
+        foreach(var toRemoveItem in toRemoveItems)
+            ind.Chromosome[toRemoveItem.index] = false;
+    }
 }
+
+
---
+++ KnapsackBruteForce/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using GeneticAlgorithm;
+using GeneticAlgorithm.Models;
+using ScottPlot;
+using Utils;
+
+// ReSharper disable InvalidXmlDocComment
+
+const string pathPrefix = "Files/dataset";
+
+var availableDataSetFiles = new List<double>() { 5, 10, 15, 20, 25, 30 };
+
+Console.WriteLine("===== Execution Started =====");
+Console.WriteLine();
+
+var plotYValues = availableDataSetFiles
+                  .Select(availableDataSet => RunBruteForceForDataSet($"{pathPrefix}{availableDataSet}").TotalSeconds)
+                  .ToList();
+
+var plt = new ScottPlot.Plot(1600, 900);
+plt.AddScatter(availableDataSetFiles.ToArray(), plotYValues.ToArray(), lineStyle: LineStyle.DashDotDot, label: "Brute Force Execution Time (Seconds)");
+plt.SaveFig($"../../../../Images/BruteForce/BruteForceExecutionTime.png");
+
+Console.WriteLine();
+Console.WriteLine("===== Execution Ended =====");
+
+// ===================================================
+
+TimeSpan BruteForceKnapsack(IEnumerable<KnapsackItem> availableItems, double maxWeight)
+{
+    var itemsList = availableItems.AsList()!;
+    var qtyOfItems = itemsList.Count;
+    var combinationsQty = Math.Pow(2, qtyOfItems);
+
+
+    var bestValue = 0.0;
+    var finalWeight = 0.0;
+    long solution = 0;
+
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+    for(long i = 0; i < combinationsQty; i++)
+    {
+        var currentWeight = 0.0;
+        var currentValue = 0.0;
+
+        for(var j = 0; j < qtyOfItems; j++)
+        {
+            var bitArray = new BitArray(BitConverter.GetBytes(i));
+
+            if(!bitArray[j]) continue;
+
+            currentWeight += itemsList[j].Weight;
+            currentValue += itemsList[j].Price;
+
+        }
+
+        if(currentWeight <= maxWeight && currentValue > bestValue)
+        {
+            bestValue = currentValue;
+            finalWeight = currentWeight;
+            solution = i;
+        }
+
+    }
+    stopwatch.Stop();
+
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.WriteLine($"Best Value found was: {bestValue}");
+    Console.WriteLine($"Solution Binary: {Convert.ToString(solution, 2).PadLeft(qtyOfItems, '0')} - ({solution})");
+    for(var i = 0; i < qtyOfItems; i++)
+        if(new BitArray(BitConverter.GetBytes(solution))[i])
+            Console.WriteLine($" => Label: {itemsList[i].Label} - Price: {itemsList[i].Price} - Weight: {itemsList[i].Weight}");
+    Console.WriteLine($"Weight of best sack: {finalWeight}");
+    Console.WriteLine($"Execution Elapsed Time: {stopwatch.Elapsed.TotalSeconds} seconds");
+    Console.WriteLine("");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.WriteLine(" ================================= ");
+    Console.ResetColor();
+    Console.WriteLine("");
+
+    return stopwatch.Elapsed;
+}
+
+TimeSpan RunBruteForceForDataSet(string path)
+{
+    /** ===========================================
+     *  Reading data from File
+        ===========================================*/
+    var (items, knapsackMaxWeight) = KnapsackDataReader.GetKnapsackData(path);
+
+    if(items is null)
+        throw new($"Error, '{nameof(items)}' cannot be null");
+
+    /** =========================================== */
+
+    return BruteForceKnapsack(items, knapsackMaxWeight);
+}
